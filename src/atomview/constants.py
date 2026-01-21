584c6eae3ce8d0e9a78549c80886e9cc2a11d34e
Charlie Harris <charlieharris001@gmail.com>
2026-01-21 14:38:34 +0000

    add metral constants

---
+++ src/atomview/constants.py
@@ -0,0 +1,8 @@
+
+
+METAL_ELEMENTS = ['ZR', 'RB', 'BA', 'OS', 'MG', 'CS', 'CA', 'TI',
+    'RH', 'NI', 'HF', 'NB', 'AL', 'GA', 'IN', 'TA',
+    'TC', 'SN', 'IR', 'CO', 'LA', 'HG', 'SC', 'MO',
+    'CR', 'K', 'W', 'RE', 'CU', 'FE', 'Y', 'SR', 'CD',
+    'AU', 'RU', 'PD', 'BE', 'PB', 'MN', 'BI', 'V', 'AG',
+    'NA', 'TL', 'PT', 'LI', 'ZN']
